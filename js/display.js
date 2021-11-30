e9eb5e3c52a9fa8ed726c29a9efb478c0ac2334c
Ali Talebi <a.ali.talebi@gmail.com>
2021-11-30 17:36:46 +0330

    v1.0.3

---
+++ js/display.js
@@ -0,0 +1,5 @@
+document.getElementById("car2").style.display="none";
+document.getElementById("car6").style.display="none";
+document.getElementById("car9").style.display="none";
+document.getElementById("car12").style.display="none";
+document.getElementById("car13").style.display="none";
